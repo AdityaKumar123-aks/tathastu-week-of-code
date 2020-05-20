6bccc735ce97eb8009b90522fa9e6afcd946143b
AdityaKumar123-aks <65608954+AdityaKumar123-aks@users.noreply.github.com>
2020-05-20 14:54:26 +0530

    Create program 1.py

---
+++ day 1/program 1.py
@@ -0,0 +1,12 @@
+#TAKE MULTIPLE INPUTS BY USER AND PRINT THOSE PARAMETERS.
+
+Name=input("ENTER YOUR NAME :")
+Branch=input("ENTER YOUR BRANCH :")
+Gender=input("ENTER YOUR GENDER :")
+College=input("ENTER YOUR COLLEGE :")
+Age=input("ENTER YOUR AGE :")
+print("Name :\n",Name)
+print("Branch :\n",Branch)
+print("Gender :\n",Gender)
+print("College :\n",College)
+print("Age :\n",Age)
